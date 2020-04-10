ee4650ead4aec46dcabdd5373c5f6175c396d925
Paul <paul@beambient.co.uk>
2020-04-10 13:15:13 +0100

    updated file to begin starting to look at this

--- src/Car.java
+++ src/Car.java
@@ -1,31 +1,22 @@
 public class Car {
-	String color = "Green";
+	
+	String color;
 	String type;
 	
-	
-	{	//initialization block
-		color = "Red";
+	{
+		color = "red";
+		type = "Celica";
 	}
 	
-	Car() { //constructor
-		color = "Blue";
+	String getDescription() {
+		String desc = "This is a " + color + " " + type;
+		return desc;
 	}
 	
-	void start() {
-		System.out.println("Get Started!");
-	}
-
-	void printDescription() {
-		System.out.println("This is a " + color + " " + type);
-	}
-	
-	public static void main(String[] args) {
-		Car myCar = new Car();
-		myCar.printDescription();
+	void customize(String newColor, String newType) 
+	{
+		color = newColor;
+		type = newType + " " + type;
+		System.out.println (getDescription());
 	}
 }
-
-// In the above code, when the main() method is called the String color and type would be initialized first(both null) then
-// the inline initialization of String Color giving us green.
-// 
-// Then it will go to the initialization block giving us color as red (because it is after the instance variables) and finally the constructor giving us blue
